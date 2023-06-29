eaac4857eec199c448ed8efc14637669f06ccfa4
ayush gupta <ayushgupta11442@gmail.com>
2023-06-30 00:54:33 +0530

    basic graph is added

---
+++ chapter1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# basic graph
+
+
+x = [1,2,3]
+y = [3,4,5]
+plt.plot(x,y) # this is plot the graph between x and y
+
+plt.title("this is our first graph ")
+
+plt.xlabel("this is x label")
+
+plt.ylabel("this is y label ")
+
+
+plt.show()
